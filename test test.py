4890f7b58181d1d61d645f18521a315e8fb3c7dd
Isabella Glaze <Isabella.Glaze@IssuerDirect.com>
2020-12-08 12:41:30 -0500

    making changes

--- test test.py
+++ test test.py
@@ -5,4 +5,4 @@ Created on Tue Dec  8 12:38:13 2020
 @author: Isabella.Glaze
 """
 
-print('hi this is a test and i am a test and test test')
+print('making changes making changes')
--- test.py
+++ test.py
@@ -5,6 +5,8 @@ Created on Fri Dec  4 10:35:09 2020
 @author: Isabella.Glaze
 """
 
+from datetime import datetime 
+
 ## this is a test file
 
 print('Hello')
@@ -12,3 +14,6 @@ print('Hello')
 ## okay but what if i change the code now ^ 
 
 print('and now i have merged both the test branch and the another-one branch')
+
+date = input('Please input start date (MM-DD-YYYY): ')
+date = datetime.strptime(date, "%m-%d-%Y").date() 
